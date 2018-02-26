11e87c3e04b138fbf009639420015bab31e4d7b5
katyjane8 <katy.jane8@gmail.com>
2018-02-26 16:32:04 -0700

    routes for auth/:provider

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#index"
+  get "/auth/:provider/callback", to: "sessions#create"
+  get 'auth/failure', to: redirect('/')
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
 
   resources :users, only: [:new, :create, :show]
 
